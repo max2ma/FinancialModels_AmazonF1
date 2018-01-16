181a6049b8d19005f7233d0ab046054f50592086
math@shaheen <liang.ma@studenti.polito.it>
2018-01-16 16:57:38 +0100

    add project files for heston model with european barrier option

---
+++ heston_model/europeanBarrierOption/hestonBarrier.cpp
@@ -0,0 +1,217 @@
+/*
+======================================================
+ Copyright 2016 Liang Ma
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+======================================================
+
+*----------------------------------------------------------------------------
+*
+* Author:   Liang Ma (liang-ma@polito.it)
+*
+* Top function is defined here with interface specified as axi4.
+* It creates an object of heston and launches the simulation.
+*
+*----------------------------------------------------------------------------
+*/
+#include "hestonBarrier.h"
+
+extern "C"
+void hestonEuroBarrier(data_t *pCall, data_t *pPut,   // call price and put price
+		data_t expect,				// theta
+		data_t kappa,				// kappa
+		data_t variance,			// xi
+		data_t correlation,			// rho
+		data_t timeT,				// time period of options
+		data_t freeRate,			// interest rate of the riskless asset
+		data_t volatility,			// volatility of the risky asset
+		data_t initPrice,			// stock price at time 0
+		data_t strikePrice,			// strike price
+		data_t upB,				// up barrier
+		data_t lowB				// low barrier
+		)
+{
+#pragma HLS INTERFACE m_axi port=pCall bundle=gmem
+#pragma HLS INTERFACE s_axilite port=pCall bundle=control
+#pragma HLS INTERFACE m_axi port=pPut bundle=gmem
+#pragma HLS INTERFACE s_axilite port=pPut bundle=control
+#pragma HLS INTERFACE s_axilite port=correlation bundle=gmem
+#pragma HLS INTERFACE s_axilite port=correlation bundle=control
+#pragma HLS INTERFACE s_axilite port=variance bundle=gmem
+#pragma HLS INTERFACE s_axilite port=variance bundle=control
+#pragma HLS INTERFACE s_axilite port=kappa bundle=gmem
+#pragma HLS INTERFACE s_axilite port=kappa bundle=control
+#pragma HLS INTERFACE s_axilite port=expect bundle=gmem
+#pragma HLS INTERFACE s_axilite port=expect bundle=control
+#pragma HLS INTERFACE s_axilite port=timeT bundle=gmem
+#pragma HLS INTERFACE s_axilite port=timeT bundle=control
+#pragma HLS INTERFACE s_axilite port=freeRate bundle=gmem
+#pragma HLS INTERFACE s_axilite port=freeRate bundle=control
+#pragma HLS INTERFACE s_axilite port=volatility bundle=gmem
+#pragma HLS INTERFACE s_axilite port=volatility bundle=control
+#pragma HLS INTERFACE s_axilite port=initPrice bundle=gmem
+#pragma HLS INTERFACE s_axilite port=initPrice bundle=control
+#pragma HLS INTERFACE s_axilite port=strikePrice bundle=gmem
+#pragma HLS INTERFACE s_axilite port=strikePrice bundle=control
+#pragma HLS INTERFACE s_axilite port=lowB bundle=gmem
+#pragma HLS INTERFACE s_axilite port=lowB bundle=control
+#pragma HLS INTERFACE s_axilite port=upB bundle=gmem
+#pragma HLS INTERFACE s_axilite port=upB bundle=control
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+
+	volData vol(expect,kappa,variance,volatility,correlation);
+	stockData sd(timeT,freeRate,volatility,initPrice,strikePrice);
+	barrierData bD(upB,lowB);
+	heston bs(sd,vol,bD);
+	data_t call,put;
+	bs.simulation(&call,&put);
+
+	*pCall=call;
+	*pPut=put;
+	return;
+}
+
+
+
+
+const int heston::NUM_RNGS=32;
+const int heston::NUM_SIMGROUPS=512;
+const int heston::NUM_STEPS=256;
+const int heston::NUM_SIMS=512;
+
+heston::heston(stockData data,volData vol,barrierData bData)
+	:data(data),vol(vol),bData(bData)
+{
+}
+void heston::simulation(data_t* pCall, data_t *pPut)
+{
+
+	RNG mt_rng[NUM_RNGS];
+#pragma HLS ARRAY_PARTITION variable=mt_rng complete dim=1
+
+	uint seeds[NUM_RNGS];
+#pragma HLS ARRAY_PARTITION variable=seeds complete dim=1
+
+	loop_seed:for(int i=0;i<NUM_RNGS;i++)
+	{
+#pragma HLS UNROLL
+		seeds[i]=i;
+	}
+	RNG::init_array(mt_rng,seeds,NUM_RNGS);
+	return sampleSIM(mt_rng,pCall,pPut);
+
+}
+void heston::sampleSIM(RNG* mt_rng, data_t* call,data_t* put)
+{
+	const data_t Dt=data.timeT/NUM_STEPS,
+			ratio1=expf(-data.freeRate*data.timeT),
+			ratio2=sqrtf(fmaxf(1.0f-vol.correlation*vol.correlation,0.0f)),
+			ratio3=Dt*data.freeRate,
+			ratio4=vol.kappa*vol.expect*Dt,
+			volInit =fmaxf(vol.initValue,0.0f)*Dt;
+
+	data_t fCall=0.0f,fPut=0.0f;
+	data_t sCall[NUM_RNGS],sPut[NUM_RNGS];
+#pragma HLS ARRAY_PARTITION variable=sCall complete dim=1
+#pragma HLS ARRAY_PARTITION variable=sPut complete dim=1
+
+	data_t stockPrice[NUM_RNGS][NUM_SIMGROUPS];
+#pragma HLS ARRAY_PARTITION variable=stockPrice complete dim=1
+
+	data_t vols[NUM_RNGS][NUM_SIMGROUPS],pVols[NUM_RNGS][NUM_SIMGROUPS];
+
+#pragma HLS ARRAY_PARTITION variable=vols complete dim=1
+#pragma HLS ARRAY_PARTITION variable=pVols complete dim=1
+
+	data_t num1[NUM_RNGS][NUM_SIMGROUPS],num2[NUM_RNGS][NUM_SIMGROUPS];
+#pragma HLS ARRAY_PARTITION variable=num2 complete dim=1
+#pragma HLS ARRAY_PARTITION variable=num1 complete dim=1
+
+	data_t bBarrier[NUM_RNGS][NUM_SIMGROUPS];
+#pragma HLS ARRAY_PARTITION variable=bBarrier complete dim=1
+
+	for(int i =0;i<NUM_RNGS;i++)
+	{
+#pragma HLS UNROLL
+		sCall[i]=0.0f;
+		sPut[i]=0.0f;
+	}
+	loop_init:for(int s=0;s<NUM_SIMGROUPS;s++)
+	{
+		for(int i =0;i<NUM_RNGS;i++)
+		{
+	#pragma HLS UNROLL
+			stockPrice[i][s]=data.initPrice;
+			vols[i][s]=vol.initValue;
+			pVols[i][s]=volInit;
+			bBarrier[i][s]=true;
+		}
+	}
+	loop_main:for(int j=0;j<NUM_SIMS;j++)
+	{
+		loop_path:for(int path=0;path<NUM_STEPS;path++)
+		{
+			loop_share:for(int s=0;s<NUM_SIMGROUPS;s++)
+			{
+		#pragma HLS PIPELINE
+				loop_parallel:for(uint i=0;i<NUM_RNGS;i++)
+				{
+#pragma HLS UNROLL
+					if(!bBarrier[i][s])
+						continue;
+					mt_rng[i].BOX_MULLER(&num1[i][s],&num2[i][s],pVols[i][s]);
+
+					stockPrice[i][s]*=expf(ratio3-pVols[i][s]*0.5f+num1[i][s]*vol.correlation+num2[i][s]*ratio2);
+					if(stockPrice[i][s]<bData.downBarrier || stockPrice[i][s]>bData.upBarrier)
+					{
+						bBarrier[i][s]=false;
+					}
+					vols[i][s]+=ratio4-vol.kappa*pVols[i][s]+vol.variance*num1[i][s];
+					pVols[i][s]=fmaxf(vols[i][s],0.0f)*Dt;
+
+				}
+			}
+		}
+		loop_sum:for(int s=0;s<NUM_SIMGROUPS;s++)
+		{
+			loop_sum_R:for(int i =0;i<NUM_RNGS;i++)
+			{
+#pragma HLS UNROLL
+				vols[i][s]=vol.initValue;
+				pVols[i][s]=volInit;
+				if(bBarrier[i][s])
+				{
+					float payoff = stockPrice[i][s]-data.strikePrice;
+					if(stockPrice[i][s]>data.strikePrice)
+					{
+						sCall[i]+= payoff;
+					}
+					else
+					{
+						sPut[i]-=payoff;
+					}
+				}
+				stockPrice[i][s]=data.initPrice;
+				bBarrier[i][s]=true;
+			}
+		}
+	}
+	loop_final_sum:for(int i =0;i<NUM_RNGS;i++)
+	{
+#pragma HLS UNROLL
+		fCall+=sCall[i];
+		fPut+=sPut[i];
+	}
+	*call= ratio1*fCall/NUM_RNGS/NUM_SIMS/NUM_SIMGROUPS;
+	*put= ratio1*fPut/NUM_RNGS/NUM_SIMS/NUM_SIMGROUPS;
+}
---
+++ heston_model/europeanBarrierOption/main.cpp
@@ -0,0 +1,210 @@
+/*
+======================================================
+ Copyright 2016 Liang Ma
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+======================================================
+*
+* Author:   Liang Ma (liang-ma@polito.it)
+*
+* Host code defining all the parameters and launching the kernel.
+*
+* ML_cl.h is the OpenCL library file <CL/cl.hpp>. Currently the version shipped with SDAccel is buggy.
+*
+* Exception handling is enabled (__CL_ENABLE_EXCEPTIONS) to make host code simpler.
+*
+* The global and local size are set to 1 since the kernel is written in C/C++ instead of OpenCL.
+*
+* Several input parameters for the simulation are defined in namespace Params
+* and can be changed by using command line options. Only the kernel name must
+* be defined.
+*
+* S0:		 stock price at time 0
+* K:		  strike price
+* rate:	interest rate
+* volatility:	 volatility of stock
+* T:		 time period of the option
+*
+*
+* callR:	-c reference value for call price
+* putR:		-p reference value for put price
+* binary_name:  -a the .xclbin binary name
+* kernel_name:  -n the kernel name
+* num_sims:					-s number of simulations
+*----------------------------------------------------------------------------
+*/
+
+#define __CL_ENABLE_EXCEPTIONS
+
+// This should be used when cl.hpp from SDAccel works.
+//#include <CL/cl.hpp>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <unistd.h>
+#include "xcl2.hpp"
+using namespace std;
+
+namespace Params
+{
+	double theta = 0.04;
+	double kappa = 0.5;
+	double xi = 1;
+	double rho = 0;
+	double S0 = 100;
+	double K = 100;
+	double rate = 0.0;
+	double volatility = 0.04;
+	double T = 1.0;
+	double upB=110;
+	double lowB=90;
+	char *kernel_name=NULL;     // -n
+	char *binary_name=NULL;     // -a
+}
+void usage(char* name)
+{
+    cout<<"Usage: "<<name
+        <<" -a opencl_binary_name"
+        <<" -n kernel_name"
+        <<" [-c call_price]"
+        <<" [-p put_price]"
+        <<endl;
+}
+int main(int argc, char** argv)
+{
+	int opt;
+	double callR=-1, putR=-1;
+	bool flaga=false,flagc=false,flagp=false,flagn=false;
+	while((opt=getopt(argc,argv,"n:a:c:p:"))!=-1){
+		switch(opt){
+			case 'n':
+				Params::kernel_name=optarg;
+				flagn=true;
+				break;
+			case 'a':
+				Params::binary_name=optarg;
+				flaga=true;
+				break;
+			case 'c':
+				callR=atof(optarg);
+				flagc=true;
+				break;
+			case 'p':
+				putR=atof(optarg);
+				flagp=true;
+				break;
+			default:
+				usage(argv[0]);
+				return -1;
+		}
+	}
+	// Check the mandatory argument.
+	if(!flagn) {
+		usage(argv[0]);
+		return -1;
+	}
+	ifstream ifstr(Params::binary_name);
+	const string programString(istreambuf_iterator<char>(ifstr),
+		(istreambuf_iterator<char>()));
+	vector<float, aligned_allocator<float> > h_call(1),h_put(1);
+
+	try
+	{
+		vector<cl::Platform> platforms;
+		cl::Platform::get(&platforms);
+
+		cl::Context context(CL_DEVICE_TYPE_ACCELERATOR);
+		vector<cl::Device> devices=context.getInfo<CL_CONTEXT_DEVICES>();
+
+		cl::Program::Binaries binaries(1, make_pair(programString.c_str(), programString.length()));
+		cl::Program program(context,devices,binaries);
+		try
+		{
+			program.build(devices);
+		}
+		catch (cl::Error err)
+		{
+			if (err.err() == CL_BUILD_PROGRAM_FAILURE)
+			{
+				string info;
+				program.getBuildInfo(devices[0],CL_PROGRAM_BUILD_LOG, &info);
+				cout << info << endl;
+				return EXIT_FAILURE;
+			}
+			else throw err;
+		}
+
+		cl::CommandQueue commandQueue(context, devices[0]);
+
+//		typedef cl::make_kernel<cl::Buffer,cl::Buffer,float,float,float,float,float,float,float,float,float,float,float> kernelType;
+//		kernelType kernelFunctor = kernelType(program, Params::kernel_name);
+    
+		cl::Kernel kernel(program,Params::kernel_name);
+    auto kernelFunctor = cl::KernelFunctor<cl::Buffer,cl::Buffer,float,float,float,float,float,float,float,float,float,float,float>(kernel);
+
+
+		cl::Buffer d_call(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+				              sizeof(float), h_call.data());
+		cl::Buffer d_put(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+					            sizeof(float), h_put.data());
+		std::vector<cl::Memory> outBufVec;
+	  outBufVec.push_back(d_call);
+		outBufVec.push_back(d_put);
+		clock_t start = clock();
+		cl::EnqueueArgs enqueueArgs(commandQueue,cl::NDRange(1),cl::NDRange(1));
+		cl::Event event = kernelFunctor(enqueueArgs,
+						d_call,d_put,
+						Params::theta,
+						Params::kappa,
+						Params::xi,
+				 		Params::rho,
+						Params::T,
+				 		Params::rate,
+						Params::volatility,
+						Params::S0,
+						Params::K,
+						Params::upB,
+						Params::lowB
+						);
+
+		commandQueue.enqueueMigrateMemObjects(outBufVec,CL_MIGRATE_MEM_OBJECT_HOST);
+		commandQueue.finish();
+		event.wait();
+
+		//cl::copy(commandQueue, d_call, h_call.begin(), h_call.end());
+		//cl::copy(commandQueue, d_put, h_put.begin(), h_put.end());
+		clock_t t = clock() - start;
+		cout << "The execution lasts for "<< (float)t /CLOCKS_PER_SEC <<" s (CPU time)."<<endl;
+		cout<<"the call price is: "<<h_call[0]<<'\t';
+		if(flagc)
+			cout<<"the difference with the reference value is "<<fabs(h_call[0]/callR-1)*100<<'%'<<endl;
+		cout<<endl;
+		cout<<"the put price is: "<<h_put[0]<<'\t';
+		if(flagp)
+			cout<<"the difference with the reference value is "<<fabs(h_put[0]/putR-1)*100<<'%'<<endl;
+		cout<<endl;
+	}
+	catch (cl::Error err)
+	{
+		cerr
+			<< "Error:\t"
+			<< err.what()
+			<< endl;
+
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
