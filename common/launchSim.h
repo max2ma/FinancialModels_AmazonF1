d8705eaa0f7bd938d82a6b564719f4846e03c7ce
math@shaheen <liang.ma@studenti.polito.it>
2018-12-07 16:24:47 +0100

    some modifications

--- common/launchSim.h
+++ common/launchSim.h
@@ -3,13 +3,12 @@
 #include "hls_stream.h"
 #include "RNG.h"
 	template<typename DATA_T>
-void prng(RNG<DATA_T> &rng0,RNG<DATA_T> &rng1, hls::stream<DATA_T> &sRNG0,hls::stream<DATA_T> &sRNG1, int nums)
-{
-	for(int i = 0 ; i < nums/4;i++){
+void prng(RNG<DATA_T> &rng0,RNG<DATA_T> &rng1, hls::stream<unsigned int> &sRNG0,hls::stream<unsigned int> &sRNG1, int nums){
+	for(int i = 0 ; i < nums>>2;i++){
 #pragma HLS PIPELINE
-		DATA_T r0, r1, r2, r3;
-		rng0.BOX_MULLER(&r0, &r1, 0, 1);
-		rng1.BOX_MULLER(&r2, &r3, 0, 1);
+		unsigned int r0, r1, r2, r3;
+		rng0.extract_number(&r0, &r1);
+		rng1.extract_number(&r2, &r3);
 		sRNG0.write(r0);
 		sRNG0.write(r2);
 		sRNG1.write(r1);
@@ -17,13 +16,28 @@ void prng(RNG<DATA_T> &rng0,RNG<DATA_T> &rng1, hls::stream<DATA_T> &sRNG0,hls::s
 	}
 }
 	template<typename DATA_T, typename Model>
-void launchSimulation(DATA_T &pCall, DATA_T &pPut, RNG<DATA_T> &rng0,RNG<DATA_T> &rng1, Model &m, int numR, int sims, int steps)
-{
+void launchSim(DATA_T &pCall, DATA_T &pPut, RNG<DATA_T> &rng0,RNG<DATA_T> &rng1, Model &m, int numR, int sims){
 #pragma HLS DATAFLOW
+	hls::stream<unsigned int> s_num0;
+	hls::stream<unsigned int> s_num1;
+#pragma HLS STREAM variable=s_num0 depth=4 dim=1
+#pragma HLS STREAM variable=s_num1 depth=4 dim=1
 	hls::stream<DATA_T> sRNG0;
 	hls::stream<DATA_T> sRNG1;
-#pragma HLS STREAM variable=sRNG0 depth=2 dim=1
-#pragma HLS STREAM variable=sRNG1 depth=2 dim=1
-	prng(rng0, rng1, sRNG0, sRNG1, numR);
+#pragma HLS STREAM variable=sRNG0 depth=4 dim=1
+#pragma HLS STREAM variable=sRNG1 depth=4 dim=1
+	
+	prng(rng0, rng1, s_num0, s_num1, numR);
+	
+	RNG<DATA_T>::BOX_MULLER(s_num0, s_num1, sRNG0, sRNG1, numR>>1);
+
 	m.simulation(sRNG0,sRNG1, sims, pCall, pPut);
+}
+	template<typename DATA_T, int SEED_STRIDE=1024, typename Model>
+void launchSimulation(DATA_T &pCall, DATA_T &pPut, int seed, Model &m, int numR, int sims){
+#pragma HLS INLINE off
+#pragma HLS ALLOCATION instances=mul limit=1 operation
+	RNG<float> rng0(seed*SEED_STRIDE);
+	RNG<float> rng1(seed*SEED_STRIDE + 1);
+	launchSim(pCall, pPut, rng0, rng1, m, numR, sims);
 }
